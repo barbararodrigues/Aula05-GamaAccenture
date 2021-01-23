52164cfb9e7d87bdb06d86db3d89ebf59f6c18c3
Bárbara Rodrigues <barbararodrigues633@gmail.com>
2021-01-22 22:01:58 -0300

    first commit

---
+++ Classes.js
@@ -0,0 +1,13 @@
+// Nome
+// Data de nascimento formato DD/MM/AAAA .split('/') pegar o indice 2 - 2021
+
+// Retorno no console a seguinte mensagem - Olá meu nome é ${name} e tenho ${age}.
+
+
+class Pessoa {
+    constructor (nome, dataNascimento) {}
+
+    getIdade(dataNascimento) {
+
+    }
+}
